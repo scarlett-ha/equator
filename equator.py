cb534b9f14692a393978f5f3de2686d7ccfbca82
Scarlett Ha <scarlett.ha@gmail.com>
2022-03-19 23:16:53 +1100

    minor fixes

--- equator.py
+++ equator.py
@@ -1,8 +1,8 @@
 import sys
 
-linenum=int(sys.argv[1])-1
+linenum = int(sys.argv[1]) - 1
 
 with open(sys.argv[2], 'r') as file:
     for i, line in enumerate(file):
         if i == linenum:
-            print(line.rstrip('\n\r]'))
+            print(line.rstrip('\n\r'))
